d25f8a3dbf1c83b7d3fc531a3123cda53cc1cebd
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-03-14 17:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function calculateMinCost() {
   //your code here
-  
-  
+  let input = document.getElementById("rope-lengths").value;
+	let arr = input.split(',');
+	let ans = [];
+	arr.forEach(element => {
+		if(element!=','){
+			ans.push(Number(element));
+		}
+	})
+  document.getElementById("result").innerHTML = typeof  ans;
   
 }  
